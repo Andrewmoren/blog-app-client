6d50e3e3499dfad4dbfb324ebbcf1e790babd6b1
Andrew <andrewmoren93@gmail.com>
2022-11-22 14:01:01 +0300

    build authorzation: logout/login

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,11 +4,18 @@ import Button from "@mui/material/Button";
 
 import styles from "./Header.module.scss";
 import Container from "@mui/material/Container";
+import { useDispatch, useSelector } from "react-redux";
+import { logout, selectIsAuth } from "../../redux/slices/auth";
 
 export const Header = () => {
-  const isAuth = false;
+  const dispatch = useDispatch();
+  const isAuth = useSelector(selectIsAuth);
 
-  const onClickLogout = () => {};
+  const onClickLogout = () => {
+    if (window.confirm("Are you sure toy want to log")) {
+      dispatch(logout());
+    }
+  };
 
   return (
     <div className={styles.root}>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { authReducer } from "./slices/auth";
 import { postsReducer } from "./slices/posts";
 
 const store = configureStore({
   reducer: {
     posts: postsReducer,
+    auth: authReducer,
   },
 });
 
