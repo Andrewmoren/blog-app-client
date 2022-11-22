1fc968509aad8c4f0f3a19583c62a746e89ec3f6
Andrew <andrewmoren93@gmail.com>
2022-11-22 09:54:48 +0300

    add fetchTags

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,16 +9,18 @@ import axios from "../axios";
 import { Post } from "../components/Post";
 import { TagsBlock } from "../components/TagsBlock";
 import { CommentsBlock } from "../components/CommentsBlock";
-import { fetchPosts } from "../redux/slices/posts";
+import { fetchPosts, fetchTags } from "../redux/slices/posts";
 
 export const Home = () => {
   const dispatch = useDispatch();
   const { posts, tags } = useSelector((state) => state.posts);
 
   const isPostsLoading = posts.status === "loading";
+  const isTagsLoading = posts.tags === "loading";
 
   useEffect(() => {
     dispatch(fetchPosts());
+    dispatch(fetchTags());
   }, []);
   return (
     <>
@@ -51,10 +53,7 @@ export const Home = () => {
           )}
         </Grid>
         <Grid xs={4} item>
-          <TagsBlock
-            items={["react", "typescript", "заметки"]}
-            isLoading={false}
-          />
+          <TagsBlock items={tags.items} isLoading={isTagsLoading} />
           <CommentsBlock
             items={[
               {
